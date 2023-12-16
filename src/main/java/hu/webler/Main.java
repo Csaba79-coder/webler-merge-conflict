85ae8824ef651c09ed28c2614cb2a313bb1d82e1
webler-biology-physiology <csabavadasz79@gmail.com>
2023-12-16 15:42:53 +0100

    add Lombok to pom.xml. and creat Dog model and call with a builder (instantiate) & called in Main

--- src/main/java/hu/webler/Main.java
+++ src/main/java/hu/webler/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Yes, it is me, Webler!");
-
-        Dog dog = new Dog("Bodri", true, 3, "brown");
+        Dog dog = Dog.builder()
+                .name("Bodri")
+                .isMale(true)
+                .age(3)
+                .color("brown")
+                .build();
         System.out.println(dog);
     }
 }
